8e1dfce9532098533a78fe5dc9fc07db6b44c9c8
Abdullah Arain <habib.arain2007@gmail.com>
2025-10-29 19:59:04 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,16 @@
+# A container for a value (Strings, Integers, Floats, Boolean) 
+# Ex: We store sugar in jar here "Jar" is variable and "Sugar" is value.
+
+name = "Abdullah Arain" 
+country = "Pakistan"
+email = "abd123@fake.com"
+
+print(name)
+print(country)
+print(email)
+
+print(" ")
+
+print(f"Assalam o Alikum! {name}")
+print(f"{country} is a great country")
+print(f"Your email {email} looks fake!")
