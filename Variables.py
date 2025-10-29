b01e637b7e8529659a0b269e3b983a7952962aaf
Abdullah Arain <habib.arain2007@gmail.com>
2025-10-29 20:06:51 +0500

    Variables in Python
    Updated comments for clarity and added alternative print method.

--- Variables.py
+++ Variables.py
@@ -1,9 +1,9 @@
 # A container for a value (Strings, Integers, Floats, Boolean) 
 # Ex: We store sugar in a jar, here "Jar" is a variable and "Sugar" is a value.
 
-name = "Abdullah Arain" # Here 'name' is a Variable and 'Abdullah Arain' is a Value.
-country = "Pakistan" # Here 'country' is a Variable and 'Pakistan' is a Value.
-email = "abd123@fake.com" # Here 'email' is a Variable and 'abd123@fake.com' is a Value.
+name = "Abdullah Arain"        # Here 'name' is a Variable and 'Abdullah Arain' is a Value.
+country = "Pakistan"           # Here 'country' is a Variable and 'Pakistan' is a Value.
+email = "abd123@fake.com"      # Here 'email' is a Variable and 'abd123@fake.com' is a Value.
 
 print(name)
 print(country)
@@ -12,4 +12,5 @@ print(email)
 # OR
 
 print(name, country, email)
+
 
