25c3bb16643f44f9093f0ce37d59a525d061bccd
Abdullah Arain <habib.arain2007@gmail.com>
2025-10-29 20:24:48 +0500

    DataTypes in Python

---
+++ 02_datatypes.py
@@ -0,0 +1,31 @@
+# strings (Values in Double "" or Single '' Qoutes)
+string1 = "Hello!"
+string2 = 'Beautifull'
+string3= "Beoble :)"
+
+# Integers are the Whole Number (can be nagetive)
+int1 = 10
+int2 = 20
+int3 = -30
+
+# Float (Number with decimals, -ve too)
+float1 = 9.99
+float2  = 19.99
+float3 = -29.99
+
+# Boolean
+online = True
+offline = False
+
+# None
+
+
+
+
+
+
+
+
+
+
+
