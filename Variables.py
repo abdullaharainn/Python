a27f2ca20e10143f299ae275d5b799629b295fe5
Abdullah Arain <habib.arain2007@gmail.com>
2025-10-29 20:04:51 +0500

    Variables.py
    Updated comments for clarity and added alternative print method.

--- Variables.py
+++ Variables.py
@@ -1,16 +1,15 @@
 # A container for a value (Strings, Integers, Floats, Boolean) 
-# Ex: We store sugar in jar here "Jar" is variable and "Sugar" is value.
+# Ex: We store sugar in a jar, here "Jar" is a variable and "Sugar" is a value.
 
-name = "Abdullah Arain" 
-country = "Pakistan"
-email = "abd123@fake.com"
+name = "Abdullah Arain" # Here 'name' is a Variable and 'Abdullah Arain' is a Value.
+country = "Pakistan" # Here 'country' is a Variable and 'Pakistan' is a Value.
+email = "abd123@fake.com" # Here 'email' is a Variable and 'abd123@fake.com' is a Value.
 
 print(name)
 print(country)
 print(email)
 
-print(" ")
+# OR
 
-print(f"Assalam o Alikum! {name}")
-print(f"{country} is a great country")
-print(f"Your email {email} looks fake!")
+print(name, country, email)
+
