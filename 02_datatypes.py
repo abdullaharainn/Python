9592bc742e4d544daa31d0a33e8929948861fb1c
Abdullah Arain <habib.arain2007@gmail.com>
2025-10-29 20:09:14 +0500

    Data Types in Python

---
+++ 02_datatypes.py
@@ -0,0 +1,13 @@
+# Integers
+# 1, 2, 3, 4, 5........10.
+
+# strings and strings are immutabble
+# Arain, Abdullah, musatafa & So on.
+
+# Float
+#1.99, 10.99, 45.99, 45.65............99.99.
+
+# Boolean
+# True or False
+
+# None
